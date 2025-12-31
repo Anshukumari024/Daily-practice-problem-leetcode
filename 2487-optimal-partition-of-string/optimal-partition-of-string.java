9986e727538dd0a92d9146f4f93c4ef715470ca8
Anshukumari024 <anshurajyadav.6639@gmail.com>
2025-12-31 16:24:24 +0530

    Time: 33 ms (36.07%) | Memory: 47 MB (42.52%) - LeetSync

---
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int partitionString(String s) {
+        HashSet<Character> set=new HashSet<>();
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(set.contains(ch)){
+                c++;
+                set.clear();
+            }
+          
+                set.add(ch);
+            
+        }
+        return c+1;
+        
+    }
+}

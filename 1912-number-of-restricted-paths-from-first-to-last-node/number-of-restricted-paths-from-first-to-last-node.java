9d54a2f4be8571f456a87c42c516126b581e0f1c
Anshukumari024 <anshurajyadav.6639@gmail.com>
2026-01-01 19:30:23 +0530

    Time: 69 ms (51.68%) | Memory: 143 MB (40.27%) - LeetSync

---
+++ 1912-number-of-restricted-paths-from-first-to-last-node/number-of-restricted-paths-from-first-to-last-node.java
@@ -0,0 +1,59 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+    List<int[]>[] graph;
+    long[] dist;
+    Integer[] dp;
+
+    public int countRestrictedPaths(int n, int[][] edges) {
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            graph[e[1]].add(new int[]{e[0], e[2]});
+        }
+        dist = new long[n + 1];
+        Arrays.fill(dist, Long.MAX_VALUE);
+        dijkstra(n);
+        dp = new Integer[n + 1];
+        return dfs(1, n);
+    }
+
+    public void dijkstra(int start) {
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[1]));
+        pq.offer(new long[]{start, 0});
+        dist[start] = 0;
+
+        while (!pq.isEmpty()) {
+            long[] cur = pq.poll();
+            int u = (int) cur[0];
+            long d = cur[1];
+
+            if (d > dist[u]) continue;
+
+            for (int[] nei : graph[u]) {
+                int v = nei[0];
+                long w = nei[1];
+                if (dist[v] > d + w) {
+                    dist[v] = d + w;
+                    pq.offer(new long[]{v, dist[v]});
+                }
+            }
+        }
+    }
+
+    public int dfs(int u, int n) {
+        if (u == n) return 1;
+        if (dp[u] != null) return dp[u];
+
+        long ways = 0;
+        for (int[] nei : graph[u]) {
+            int v = nei[0];
+            if (dist[u] > dist[v]) {
+                ways = (ways + dfs(v, n)) % MOD;
+            }
+        }
+
+        return dp[u] = (int) ways;
+    }
+}

3de422ab5822317ef40924216551463713c3e9c7
Anshukumari024 <anshurajyadav.6639@gmail.com>
2026-01-27 09:29:24 +0530

    Time: 155 ms (28.74%) | Memory: 275.7 MB (37.24%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -7,22 +7,17 @@ class Solution {
             this.cost = cost;
         }
     }
-
     public int minCost(int n, int[][] edges) {
         List<List<Pair>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-
         for (int[] e : edges) {
             int u = e[0], v = e[1], w = e[2];
-            graph.get(u).add(new Pair(v, w));        // forward
-            graph.get(v).add(new Pair(u, 2 * w));    // backward
+            graph.get(u).add(new Pair(v, w));       
+            graph.get(v).add(new Pair(u, 2 * w));    
         }
-
         int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[0] = 0;
-
-        boolean[] visited = new boolean[n];
 
         PriorityQueue<Pair> pq =
             new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
@@ -33,23 +28,19 @@ class Solution {
             int u = cur.node;
             int d = cur.cost;
 
-            // 🔴 already visited → skip
-            if (visited[u]) continue;
-
-            // 🔒 lock this node forever
-            visited[u] = true;
-
-            if (u == n - 1) return d; // early exit
+            if (d > dist[u]) continue;
 
             for (Pair nei : graph.get(u)) {
                 int v = nei.node;
-                if (!visited[v] && d + nei.cost < dist[v]) {
-                    dist[v] = d + nei.cost;
-                    pq.add(new Pair(v, dist[v]));
+                int newCost = d + nei.cost;
+
+                if (newCost < dist[v]) {
+                    dist[v] = newCost;
+                    pq.add(new Pair(v, newCost));
                 }
             }
         }
 
-        return -1;
+        return dist[n - 1] == Integer.MAX_VALUE ? -1 : dist[n - 1];
     }
 }

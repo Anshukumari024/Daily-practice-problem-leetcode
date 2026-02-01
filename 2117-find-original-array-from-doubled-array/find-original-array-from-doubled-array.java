de4d496ae67a9b546a029885adf493cde996aa6f
Anshukumari024 <anshurajyadav.6639@gmail.com>
2026-02-01 23:17:51 +0530

    Time: 61 ms (87.64%) | Memory: 141.4 MB (57.83%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -1,27 +1,14 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        int n = changed.length;
-        if (n % 2 != 0) return new int[0];
-        Arrays.sort(changed);
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int x : changed) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
-        }
-        int[] ans = new int[n / 2];
-        int idx = 0;
-        for (int x : changed) {
-            if (map.get(x) == 0) continue;
-
-            int twice = x * 2;
-            if (!map.containsKey(twice) || map.get(twice) == 0) {
-                return new int[0];
-            }
-            if(idx<n/2)
-            ans[idx++] = x;
-            map.put(x, map.get(x) - 1);
-            map.put(twice, map.get(twice) - 1);
-        }
-
-        return ans;
+    int[] temp=new int[changed.length/2];
+    Queue<Integer> x=new LinkedList<>();
+    Arrays.sort(changed);
+    int i=0;
+    for(int num:changed){
+        if(!x.isEmpty() && x.peek()==num)
+            temp[i++]=x.poll()/2;
+        else x.add(num*2);
     }
+    return x.size()>0 ? new int[]{}:temp;
 }
+    }

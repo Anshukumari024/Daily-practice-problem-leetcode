f4298c9b174801beb032276a1a72a925209d6f59
Anshukumari024 <anshurajyadav.6639@gmail.com>
2026-03-07 10:03:16 +0530

    Time: 31 ms (28.81%) | Memory: 46.5 MB (99.58%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,39 +1,32 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        s = s + s;
-
-        StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
-
-        for(int k = 0; k < 2*n; k++){
-            s1.append(k%2==1 ? '0':'1');
-            s2.append(k%2==1 ? '1':'0');
-        }
-
-        int c1 = 0, c2 = 0;
         int ans = Integer.MAX_VALUE;
-
+        int c1 = 0;
+        int c2 = 0;
         int i = 0, j = 0;
+        while (j < 2 * n) {
+            char exp1 = (j % 2 == 1) ? '1' : '0';
+            char exp2 = (j % 2 == 1) ? '0' : '1';
+            if (s.charAt(j % n) != exp1)
+                c1++;
+            if (s.charAt(j % n) != exp2)
+                c2++;
+            if (j - i + 1 > n) {
+                exp1 = (i % 2 == 1) ? '1' : '0';
+                exp2 = (i % 2 == 1) ? '0' : '1';
 
-        while(j < 2*n){
-
-            if(s.charAt(j) != s1.charAt(j)) c1++;
-            if(s.charAt(j) != s2.charAt(j)) c2++;
-
-            if(j-i+1 > n){
-                if(s.charAt(i) != s1.charAt(i)) c1--;
-                if(s.charAt(i) != s2.charAt(i)) c2--;
+                if (s.charAt(i % n) != exp1)
+                    c1--;
+                if (s.charAt(i % n) != exp2)
+                    c2--;
                 i++;
             }
-
-            if(j-i+1 == n){
+            if (j - i + 1 == n)
                 ans = Math.min(ans, Math.min(c1, c2));
-            }
 
             j++;
         }
-
         return ans;
     }
 }

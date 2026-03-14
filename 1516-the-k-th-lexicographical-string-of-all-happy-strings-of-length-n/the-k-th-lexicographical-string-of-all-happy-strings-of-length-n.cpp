657f59b11d09397257f9ddb230f0185cd2462904
Anshu Kumari <anshurajyadav.6639@gmail.com>
2026-03-14 08:57:08 +0530

    Time: 3 ms (59.10%) | Memory: 9 MB (58.31%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string ans="";
+    int k;
+    string getHappyString(int n, int k) {
+        this->k=k;
+        solve(n,"");
+        return ans;
+    }
+    void solve(int n,string curr){
+        if(curr.length()==n){
+            k--;
+            if(k==0) ans=curr;
+            return;
+        }
+        for(char ch='a';ch<='c';ch++){
+            if(curr.length()>0 && curr[curr.length()-1]==ch) continue;
+            solve(n,curr+ch);
+            if(k==0) return;
+        }
+    }
+};

a14c45203fbef2a1513873e08ea8b1614f4e8bee
Anshu Kumari <anshurajyadav.6639@gmail.com>
2026-03-17 11:28:12 +0530

    Time: 7 ms (60.60%) | Memory: 47.1 MB (74.13%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,32 +1,30 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int n=numCourses;
-        List<List<Integer>> adj=new ArrayList<>();
-        List<Integer> ans=new ArrayList<>();
-        int[] in=new int[n];
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+    public boolean canFinish(int numCourses, int[][] p) {
+        int n=p.length;
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
         }
-        for(int[] pre:prerequisites){
-            int course=pre[0];
-            int preq=pre[1];
-            in[course]++;
-            adj.get(preq).add(course);
+        int[] in=new int[numCourses];
+        for(int i=0;i<n;i++){
+            int a=p[i][0];
+            int b=p[i][1];
+            in[a]++;
+            graph.get(b).add(a);
         }
         Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<in.length;i++){
             if(in[i]==0) q.add(i);
         }
+        int c=0;
         while(!q.isEmpty()){
-            int curr=q.poll();
-            ans.add(curr);
-
-            for(int neigh:adj.get(curr)){
-                in[neigh]--;
-                if(in[neigh]==0) q.add(neigh);
+            int rv=q.poll();
+            c++;
+            for(int neig:graph.get(rv)){
+                in[neig]--;
+                if(in[neig]==0) q.add(neig);
             }
         }
-        return ans.size()==n;
-        
+        return c==numCourses;
     }
 }
